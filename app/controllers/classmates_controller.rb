76157e20763ce5af2f335f066cbdb0098d314226
Matt <mvaden08@gmail.com>
2016-06-01 10:52:06 -0400

    Added a show view for classmates.

--- app/controllers/classmates_controller.rb
+++ app/controllers/classmates_controller.rb
@@ -1,15 +1,21 @@
 class ClassmatesController < ApplicationController
+  def show
+    @classmate = Classmate.find(params[:id])
+  end
+
   def new
   end
 
   def create
-     @classmate = Classmate.new(classmate_params)
+    @classmate = Classmate.new(classmate_params)
 
-     @classmate.save
-     redirect_to @classmate
+    @classmate.save
+    redirect_to @classmate
   end
 
   private
     def classmate_params
       params.require(:classmate).permit(:first_name, :last_name, :portfolio_url, :location)
     end
+    
+  end
