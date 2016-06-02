dd71ef2b25c62006d91b6a49fe6ae3c77c33d19c
Matt <mvaden08@gmail.com>
2016-06-02 16:22:10 -0400

    Changed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   # root 'welcome#index'
-  get 'classmates/index'
+  root 'classmates#index'
   # You can have the root of your site routed with "root"
 end
 
