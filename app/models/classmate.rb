0c9f28320a66b39a4e237bf665659f09b3c2d765
Matt <mvaden08@gmail.com>
2016-06-01 21:52:16 -0400

    Generated second model for comments and ran migration.

--- app/models/classmate.rb
+++ app/models/classmate.rb
@@ -1,4 +1,5 @@
 class Classmate < ActiveRecord::Base
+   has_many :comments
    validates :first_name, presence: true,
                     length: { minimum: 3 }
    validates :last_name, presence: true,
